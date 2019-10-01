7765d722835034b54a01b4d442c6a60015420b90
Keisuke Horota <keisuke.horota@gmail.com>
2019-10-01 09:36:17 -0700

    Optimize functions

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const fetch = require('isomorphic-unfetch');
 const parse5 = require('parse5');
 
+const imageWrapperIds = [
+  'img-canvas',
+  'imgTagWrapperId',
+  'digitalMusicProductImage_feature_div',
+]
+
+const searchTree = (node, targetIds) => {
+  const targetWrapper = node.childNodes && node.childNodes.find(_node => _node.attrs && _node.attrs.some(attr => attr.name === 'id' && targetIds.indexOf(attr.value) >= 0));
+  if (targetWrapper) {
+    return targetWrapper;
+  } else if (node.childNodes != null) {
+    let i;
+    let result = null;
+    for (let i = 0; result == null && i < node.childNodes.length; i += 1){
+      result = searchTree(node.childNodes[i], targetIds);
+    }
+    return result;
+  }
+  return null;
+}
+
 const amazonMetadataParser = (url) => {
   return new Promise((resolve, reject) => {
     fetch(url, {
@@ -11,47 +32,9 @@ const amazonMetadataParser = (url) => {
       .then(response => response.text())
       .then((html) => {
         const document = parse5.parse(html);
-        let product = document.childNodes
-          .find(node => node.tagName === 'html')
-          .childNodes.find(node => node.tagName === 'body')
-          .childNodes.find(node => node.attrs.some(attr => attr.name === 'id' && attr.value === 'a-page'))
 
-        let image;
-
-        if (product.childNodes.some(node => node.attrs && node.attrs.some(attr => attr.name === 'id' && attr.value === 'dp'))) {
-          product = product.childNodes.find(node => node.attrs && node.attrs.some(attr => attr.name === 'id' && attr.value === 'dp'))
-          .childNodes.find(node => node.attrs && node.attrs.some(attr => attr.name === 'id' && attr.value === 'dp-container'))
-          .childNodes.find(node => node.attrs && node.attrs.some(attr => attr.name === 'id' && attr.value === 'ppd'))
-
-          image = product.childNodes.find(node => node.attrs && node.attrs.some(attr => attr.name === 'id' && attr.value === 'leftCol'))
-            .childNodes.find(node => node.attrs && node.attrs.some(attr => attr.name === 'id' && attr.value === 'imageBlock_feature_div'))
-            .childNodes.find(node => node.attrs && node.attrs.find(attr => attr.name === 'id' && attr.value === 'imageBlock'))
-            .childNodes.find(node => node.tagName === 'div')
-            .childNodes.find(node => node.tagName === 'div')
-            .childNodes.find(node => node.attrs && node.attrs.some(attr => attr.name === 'class' && attr.value === 'a-text-center a-fixed-left-grid-col a-col-right'))
-            .childNodes.find(node => node.attrs && node.attrs.some(attr => attr.name === 'class' && attr.value.match(/canvas/)))
-            .childNodes.find(node => node.attrs && node.attrs.some(attr => attr.name === 'id' && attr.value === 'main-image-container'))
-            .childNodes.find(node => node.tagName === 'ul')
-            .childNodes.find(node => node.tagName === 'li')
-            .childNodes.find(node => node.tagName === 'span')
-            .childNodes.find(node => node.tagName === 'span')
-            .childNodes.find(node => node.tagName === 'div')
-            .childNodes.find(node => node.tagName === 'img')
-        } else if (product.childNodes.some(node => node.attrs && node.attrs.some(attr => attr.name === 'class' && attr.value.match(/DigitalMusicDetailPage/) && attr.value.match(/PrimeMusic/)))) {
-          // Digital music
-          product = product.childNodes.find(node => node.attrs && node.attrs.some(attr => attr.name === 'class' && attr.value.match(/DigitalMusicDetailPage/) && attr.value.match(/PrimeMusic/)))
-          .childNodes.find(node => node.attrs && node.attrs.some(attr => attr.name === 'id' && attr.value === 'divsinglecolumnminwidth'))
-          .childNodes.find(node => node.attrs && node.attrs.some(attr => attr.name === 'id' && attr.value === 'ppd'))
-
-          image = product.childNodes.find(node => node.attrs && !node.attrs.some(attr => attr.name === 'class' && attr.value === 'a-row'))
-            .childNodes.find(node => node.attrs && node.attrs.some(attr => attr.name === 'class' && attr.value === 'a-fixed-right-grid a-spacing-large'))
-            .childNodes.find(node => node.attrs && node.attrs.some(attr => attr.name === 'class' && attr.value === 'a-fixed-right-grid-inner'))
-            .childNodes.find(node => node.attrs && node.attrs.some(attr => attr.name === 'class' && attr.value === 'a-fixed-right-grid-col a-col-left'))
-            .childNodes.find(node => node.attrs && node.attrs.some(attr => attr.name === 'class' && attr.value === 'a-row a-grid-vertical-align a-grid-top a-ws-row'))
-            .childNodes.find(node => node.attrs && node.attrs.some(attr => attr.name === 'id' && attr.value === 'ppd-left'))
-            .childNodes.find(node => node.attrs && node.attrs.some(attr => attr.name === 'id' && attr.value === 'digitalMusicProductImage_feature_div'))
-            .childNodes.find(node => node.tagName === 'img')
-        }
+        const imageWrapper = searchTree(document, imageWrapperIds);
+        const image = imageWrapper.childNodes.find(node => node.tagName === 'img');
 
         resolve({
           title: image.attrs.find(attr => attr.name === 'alt').value,
